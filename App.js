41daa782fb45427b91cfbb4a1c3c241024a92d6d
terrencewang <terrence.wang.98@gmail.com>
2021-05-03 12:18:44 -0700

    testing github

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Changing on atom this time!</Text>
+      <Text>Making yet another change!</Text>
       <StatusBar style="auto" />
     </View>
   );

26f6e54d0615117642785208ead7fcd5e3eb026d
terrencewang <terrence.wang.98@gmail.com>
2021-05-03 15:48:24 -0700

    color testing

---
+++ src/components/homeScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.welcomeText}>Making another change to test things!</Text>
+      <StatusBar
+        barStyle = "light-content"
+        backgroundColor = '#000'/>
+    </View>
+  );
+}
+
+//https://reactnative.dev/docs/style
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#325F71',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcomeText: {
+    fontFamily: arial,
+    color: '#fff'
+  }
+});

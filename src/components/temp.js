6c24dde810e20f334905dd2b32731c1cf309fcd1
Vincent Wang <vincent.wang.60@gmail.com>
2021-05-05 22:56:48 -0700

    text input and log button

--- src/components/temp.js
+++ src/components/temp.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect }  from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, TouchableOpacity } from 'react-native';
 import {
   useFonts,
   Inter_300Light,
 } from '@expo-google-fonts/inter';
 
+import Amplify, { Auth } from "aws-amplify"
+import AWSConfig from '../../aws-exports'
+Amplify.configure(AWSConfig)
+
+
 export default function temp() {
+  const [username, setUsername] = useState('');
+  function signUp(gUsername)  {
+    console.log(gUsername);
+  }
   return (
     <View style={styles.container}>
-      <Text style = {[styles.welcomeText]}>Temp text</Text>
+      <TextInput
+        label="Username"
+        placeholder="empty"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+        style={styles.welcomeText}
+      />
+    <TouchableOpacity onPress={() => signUp(username)}>
+         <Text style = {[styles.buttonText]}>
+             Login
+         </Text>
+      </TouchableOpacity >
       <StatusBar
         barStyle = "light-content"
         backgroundColor = '#000'/>
@@ -28,5 +48,15 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: '#fff',
     fontFamily: 'Inter_300Light'
+  },
+  buttonText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderColor: 'black',
+    backgroundColor: '#AACCDA',
+    fontSize: 20,
+    color: 'black',
+    fontFamily: 'Inter_600SemiBold',
   }
 });
